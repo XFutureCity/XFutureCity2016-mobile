553fce90ac4c459a24c9723760000208ea2b5b76
Caylus Michael <mcaylus@xebia.fr>
2016-04-03 11:55:35 +0200

    Improve UI layout on detail view

--- flare-sdk/Trilateral/Trilateral/CatalogDetailViewController.swift
+++ flare-sdk/Trilateral/Trilateral/CatalogDetailViewController.swift
@@ -15,6 +15,20 @@ class CatalogDetailViewController : UIViewController {
     var thing: Thing!
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var line3: UILabel!
+    @IBOutlet weak var line7: UILabel!
+    @IBOutlet weak var line8: UILabel!
+    
+    override func viewDidLoad() {
+        self.line3.layer.borderWidth = 2
+        self.line3.layer.borderColor = UIColor.whiteColor().CGColor
+        self.line7.layer.borderWidth = 2
+        self.line7.layer.borderColor = UIColor.whiteColor().CGColor
+        self.line8.layer.borderWidth = 2
+        self.line8.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+    
+    
     @IBAction func didSelectCloseButton() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
