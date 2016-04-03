c2a446083c58f49f947e6222cf4af717106e583b
Simone Civetta <viteinfinite@gmail.com>
2016-04-03 02:56:18 +0200

    Add new cursor

--- flare-sdk/Trilateral/Trilateral/CatalogCell.swift
+++ flare-sdk/Trilateral/Trilateral/CatalogCell.swift
@@ -22,17 +22,33 @@ class CatalogCell : UICollectionViewCell {
     @IBOutlet weak var cardContainer: UIView!
     @IBOutlet weak var compass: DirectionView!
     
+    private weak var overlayCursor: UIImageView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let image = UIImage(named: "compass.png")
+        let renderedImage = image?.imageWithRenderingMode(.AlwaysTemplate)
+        compass.image = renderedImage
+        let overlay = UIImageView(image: UIImage(named: "compass-cursor@2x.png"))
+        overlay.contentMode = .ScaleToFill
+        overlay.frame = compass.bounds
+        overlayCursor = overlay
+        compass.addSubview(overlay)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let overlayCursor = overlayCursor {
+            overlayCursor.frame = compass.bounds
+        }
+    }
+    
     func update(thing: Thing, device: Device) {
         nameLabel.text = thing.name
         cardContainer.backgroundColor = thing.uiColor
         nameLabel.textColor = thing.uiColor == UIColor.whiteColor() ? .blackColor() : .whiteColor()
         subnameLabel.textColor = nameLabel.textColor
-        
-        let image = UIImage(named: "compass.png")
-        let renderedImage = image?.imageWithRenderingMode(.AlwaysTemplate)
-        compass.image = renderedImage
         compass.tintColor = thing.uiColor
-        
         let rads = degreesToRadians(device.angleTo(thing)) - degreesToRadians(device.angle())
         compass.angle = rads
     }
